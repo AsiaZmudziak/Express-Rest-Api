5a5b1205e94a6f85e032f011e3e1ffcef670e511
unknown <asiazmudziak@gmail.com>
2021-05-16 22:19:40 +0200

    add seats counter

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -1,67 +1,88 @@
 import React from 'react';
 import io from 'socket.io-client';
 import { Button, Progress, Alert } from 'reactstrap';
-
 import './SeatChooser.scss';
-
 class SeatChooser extends React.Component {
-
-  startInterval() {
-    const { loadSeats } = this.props;
-    setInterval(loadSeats, 120000);
-  }
-
-  stopInterval() {
-    clearInterval(this.startInterval);
-  }
-  
   componentDidMount() {
     if (process.env.NODE_ENV === 'production') {
       this.socket = io.connect();
     } else {
       this.socket = io.connect('http://localhost:8000');
     }
-    
-    const { loadSeats } = this.props;
+    const { loadSeats, loadSeatsData } = this.props;
     loadSeats();
-    this.startInterval();
+    this.socket.on('seatsUpdated', seats => {
+      loadSeatsData(seats);
+    });
   }
-
-  componentWillUnmount() {
-    this.stopInterval();
-  }
-
-  isTaken = (seatId) => {
+  isTaken = seatId => {
     const { seats, chosenDay } = this.props;
-
-    return (seats.some(item => (item.seat === seatId && item.day === chosenDay)));
-  }
-
-  prepareSeat = (seatId) => {
+    return seats.some(item => item.seat === seatId && item.day === chosenDay);
+  };
+  prepareSeat = seatId => {
     const { chosenSeat, updateSeat } = this.props;
     const { isTaken } = this;
+    if (seatId === chosenSeat)
+      return (
+        <Button key={seatId} className='seats__seat' color='primary'>
+          {seatId}
+        </Button>
+      );
+    else if (isTaken(seatId))
+      return (
+        <Button key={seatId} className='seats__seat' disabled color='secondary'>
+          {seatId}
+        </Button>
+      );
+    else
+      return (
+        <Button
+          key={seatId}
+          color='primary'
+          className='seats__seat'
+          outline
+          onClick={e => updateSeat(e, seatId)}>
+          {seatId}
+        </Button>
+      );
+  };
 
-    if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
-    else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
-    else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
-  }
+  takenSeats = seats => {
+    const seatsTaken = seats.filter(i => this.isTaken(i.seat));
+    return seatsTaken.length;
+  };
 
   render() {
-
-    const { prepareSeat } = this;
-    const { requests } = this.props;
+    const { prepareSeat, takenSeats } = this;
+    const { requests, seats } = this.props;
+    const allSeats = 50;
+    let freeSeats = allSeats - takenSeats(seats);
 
     return (
       <div>
         <h3>Pick a seat</h3>
-        <small id="pickHelp" className="form-text text-muted ml-2"><Button color="secondary" /> – seat is already taken</small>
-        <small id="pickHelpTwo" className="form-text text-muted ml-2 mb-4"><Button outline color="primary" /> – it's empty</small>
-        { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success) && <div className="seats">{[...Array(50)].map((x, i) => prepareSeat(i+1) )}</div>}
-        { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending) && <Progress animated color="primary" value={50} /> }
-        { (requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error) && <Alert color="warning">Couldn't load seats...</Alert> }
+        <small id='pickHelp' className='form-text text-muted ml-2'>
+          <Button color='secondary' /> – seat is already taken
+        </small>
+        <small id='pickHelpTwo' className='form-text text-muted ml-2 mb-4'>
+          <Button outline color='primary' /> – it's empty
+        </small>
+        {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].success && (
+          <div className='seats'>
+            {[...Array(allSeats)].map((x, i) => prepareSeat(i + 1))}
+          </div>
+        )}
+        {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].pending && (
+          <Progress animated color='primary' value={50} />
+        )}
+        {requests['LOAD_SEATS'] && requests['LOAD_SEATS'].error && (
+          <Alert color='warning'>Couldn't load seats...</Alert>
+        )}
+        <p>
+          Free seats: {freeSeats}/{allSeats}
+        </p>
       </div>
-    )
-  };
+    );
+  }
 }
-
 export default SeatChooser;
